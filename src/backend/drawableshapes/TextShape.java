833fa78972e4ad3d312c4059b9bcaa8524afc32e
babhero10 <abdallah.elsaid25@gmail.com>
2022-11-21 18:00:05 +0200

    Add Text Shape

---
+++ src/backend/drawableshapes/TextShape.java
@@ -0,0 +1,54 @@
+package backend.drawableshapes;
+
+import backend.struct.OtherShape;
+import backend.struct.Shape;
+
+import java.awt.*;
+import java.util.HashMap;
+
+import static backend.constants.Properties.SET_FILL_KEY;
+
+public class DrawingShape extends OtherShape {
+    private
+
+    public DrawingShape() {
+        super();
+        getProperties().put(SET_FILL_KEY, "false");
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    @Override
+    public void setFillColor(Color color) {
+
+    }
+
+    @Override
+    public Color getFillColor() {
+        return null;
+    }
+
+    @Override
+    public void draw(Graphics canvas) {
+        ((Graphics2D)canvas).setStroke(new BasicStroke(DEF_STROKE_SIZE));
+        canvas.setColor(getColor());
+        canvas.drawLine(getPosition().x, getPosition().y, getEndPoint().x, getEndPoint().y);
+    }
+
+    @Override
+    public Shape copy(Shape shape) {
+        LineSegment c = new LineSegment();
+        c.setEndPoint(getEndPoint());
+        c.setColor(getColor());
+        c.setFillColor(getFillColor());
+        c.setPosition((Point) getPosition().clone());
+        c.setProperties(new HashMap<>(getProperties()));
+        return c;
+    }
+}
---
+++ src/frontend/windows/shapeswindow/TextDrawWindow.java
@@ -0,0 +1,199 @@
+package frontend.windows.shapeswindow;
+
+
+import backend.drawableshapes.Rectangle;
+import backend.exception.InvalidName;
+import backend.Engine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static backend.constants.Properties.*;
+
+public class RectangleWindow extends JDialog {
+
+    private final Engine engine;
+    private final JTextField nameField, xPosField, yPosField, widthField, heightField;
+
+    private final Rectangle rectangle;
+
+    private final JCheckBox strokeEnableCheck, fillEnableCheck;
+
+    JLabel fillColorLabel, colorLabel;
+    JButton fillColorBtn, colorBtn;
+
+    public RectangleWindow(JFrame parent, Engine engine) {
+        super(parent, "Rectangle probleties", ModalityType.DOCUMENT_MODAL);
+        this.engine = engine;
+        rectangle = new Rectangle();
+
+        this.setLayout(null);
+        this.setSize(500, 580);
+
+        JLabel shapeNameLabel = new JLabel("Shape name");
+        nameField = new JTextField();
+
+        JLabel xPosLabel = new JLabel("X position");
+        xPosField = new JTextField();
+
+        JLabel yPosLabel = new JLabel("Y position");
+        yPosField = new JTextField();
+
+        JLabel widthLabel = new JLabel("Width ");
+        widthField = new JTextField();
+
+        JLabel heightLabel = new JLabel("Height ");
+        heightField = new JTextField();
+
+        colorLabel = new JLabel("  ");
+        colorBtn = new JButton("Stroke");
+
+        fillColorLabel = new JLabel("  ");
+        fillColorBtn = new JButton("Fill");
+
+        colorLabel = new JLabel("  ");
+        colorBtn = new JButton("Stroke");
+        strokeEnableCheck = new JCheckBox("Stroke enable",true);
+        strokeEnableCheck.addActionListener(e->strokeCheckChanged());
+
+        fillColorLabel = new JLabel("  ");
+        fillColorBtn = new JButton("Fill");
+        fillEnableCheck = new JCheckBox("Fill enable", true);
+        fillEnableCheck.addActionListener(e->fillCheckChanged());
+
+        JButton drawBtn = new JButton("Draw");
+
+        shapeNameLabel.setBounds(50, 50, 100, 50);
+        nameField.setBounds(150, 50, 250, 50);
+
+        xPosLabel.setBounds(50, 50*2, 100, 50);
+        xPosField.setBounds(150, 50*2, 250, 50);
+
+        yPosLabel.setBounds(50, 50*3, 100, 50);
+        yPosField.setBounds(150, 50*3, 250, 50);
+
+        widthLabel.setBounds(50, 50*4, 100, 50);
+        widthField.setBounds(150, 50*4 , 250, 50);
+
+        heightLabel.setBounds(50, 50*5, 100, 50);
+        heightField.setBounds(150, 50*5 , 250, 50);
+
+        colorBtn.setBounds(100, 50*6 + 20, 100, 50);
+        colorLabel.setBounds(200, 50*6 + 20, 125, 50);
+        strokeEnableCheck.setBounds(350, 50*6+25, 150, 50);
+
+        fillColorBtn.setBounds(100, 50*7 + 20, 100, 50);
+        fillColorLabel.setBounds(200, 50*7 + 20, 125, 50);
+        fillEnableCheck.setBounds(350, 50*7+25, 150, 50);
+
+        drawBtn.setBounds(225,50*8 + 50,100,50);
+
+        colorBtn.setFocusable(false);
+        colorBtn.addActionListener(e -> setColor());
+        fillColorBtn.setFocusable(false);
+        fillColorBtn.addActionListener(e -> setFillColor());
+
+        drawBtn.setFocusable(false);
+        drawBtn.addActionListener(actionEvent -> draw());
+        colorLabel.setOpaque(true);
+        colorLabel.setBackground(Color.BLACK);
+
+        fillColorLabel.setOpaque(true);
+        fillColorLabel.setBackground(Color.BLACK);
+
+        this.add(shapeNameLabel);
+        this.add(nameField);
+        this.add(xPosLabel);
+        this.add(xPosField);
+        this.add(yPosLabel);
+        this.add(yPosField);
+        this.add(widthLabel);
+        this.add(widthField);
+        this.add(heightLabel);
+        this.add(heightField);
+        this.add(colorLabel);
+        this.add(colorBtn);
+        this.add(fillColorLabel);
+        this.add(fillColorBtn);
+        this.add(strokeEnableCheck);
+        this.add(fillEnableCheck);
+        this.add(drawBtn);
+        this.getRootPane().setDefaultButton(drawBtn);
+
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+    public void setColor(){
+        Color color = JColorChooser.showDialog(this, "Pick a color", Color.BLACK);
+        if (color == null) return;
+        colorLabel.setBackground(color);
+        rectangle.setColor(color);
+    }
+
+    public void setFillColor(){
+        Color color = JColorChooser.showDialog(this, "Pick a color", Color.BLACK);
+        if (color == null) return;
+        fillColorLabel.setBackground(color);
+        rectangle.setFillColor(color);
+    }
+
+    public void draw() {
+        int x, y;
+        int width, height;
+        boolean stroke, fill;
+        try {
+            x = Integer.parseInt(xPosField.getText().trim());
+            y = Integer.parseInt(yPosField.getText().trim());
+            width = Integer.parseInt(widthField.getText().trim());
+            height = Integer.parseInt(heightField.getText().trim());
+            stroke = strokeEnableCheck.isSelected();
+            fill = fillEnableCheck.isSelected();
+
+            if (!(fill || stroke)) {
+                JOptionPane.showMessageDialog(null, "Requered At least one check ",
+                        "Invalid data!",
+                        JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            if (nameField.getText().trim().equals(""))
+                throw new NumberFormatException();
+
+            engine.checkShapeName(nameField.getText());
+
+            Map<String, String> p = new HashMap<>();
+            p.put(NAME_KEY, nameField.getText());
+            p.put(SET_BORDER_KEY, String.valueOf(stroke));
+            p.put(SET_FILL_KEY, String.valueOf(fill));
+            rectangle.setProperties(p);
+            rectangle.setPosition(new Point(x, y));
+            rectangle.setWidth(width);
+            rectangle.setHeight(height);
+            engine.addShape(rectangle);
+
+            engine.refresh(null);
+            this.dispose();
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid data", "Failed!",
+                                    JOptionPane.WARNING_MESSAGE);
+        } catch (InvalidName e) {
+            JOptionPane.showMessageDialog(null, "Name is already been used", "Invalid name!",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    private void strokeCheckChanged() {
+        colorBtn.setEnabled(strokeEnableCheck.isSelected());
+    }
+    private void fillCheckChanged() {
+        fillColorBtn.setEnabled(fillEnableCheck.isSelected());
+    }
+}
+
+
